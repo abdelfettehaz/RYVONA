31683d78f4245e4eae813e34cbc135388baacf2b
abdelfetteh <abdelfettehazouz838@gmail.com>
2025-09-23 13:07:27 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost',
+        target: 'https://ryvona.xyz',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/project/api'),
         configure: (proxy, options) => {
