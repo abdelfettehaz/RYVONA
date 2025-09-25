4e851b5ccc8f510155846d974e8991353d617a88
abdelfetteh <abdelfettehazouz838@gmail.com>
2025-09-25 12:14:20 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -106,7 +106,7 @@ const Navbar: React.FC = () => {
         <div className="flex justify-between items-center h-17">
           <Link to="/" className="flex items-center">
             <img 
-              src="src/components/image1.png"
+              src="image1.png"
               alt="RYVONA DESIGN YOUR WAY"
               className="w-24 h-16 sm:w-32 sm:h-20 object-contain"
             />
